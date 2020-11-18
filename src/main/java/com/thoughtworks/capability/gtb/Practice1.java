7e54caf1baba60db0cbe5df28306346794512e36
SONGYI-chen123 <song1577660501@163.com>
2020-11-18 18:26:47 +0800

    [syc]:finished Practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -2,6 +2,8 @@ package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
 
+import static java.time.temporal.ChronoUnit.DAYS;
+
 /**
  * 计算任意日期与下一个劳动节相差多少天
  *
@@ -10,7 +12,14 @@ import java.time.LocalDate;
  */
 public class Practice1 {
 
-  public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
-  }
+    public static long getDaysBetweenNextLaborDay(LocalDate date) {
+        LocalDate laborDayofSameYear = LocalDate.of(date.getYear(), 5, 1);
+        if (laborDayofSameYear.isAfter(date)) {
+            return date.until(laborDayofSameYear, DAYS);
+        } else {
+            int nextYear = date.getYear() + 1;
+            LocalDate laborDayofNextYear = LocalDate.of(nextYear, 5, 1);
+            return date.until(laborDayofNextYear, DAYS);
+        }
+    }
 }
